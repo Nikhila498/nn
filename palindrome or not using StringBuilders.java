fc74ce94c66e7920f8eac9abd2c57fcef769b350
Nikhila498 <gajananikhila@gmail.com>
2025-06-05 13:51:49 +0530

    Add files via upload

---
+++ palindrome or not using StringBuilders.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("enter a string:");
+	    String input=sc.nextLine();
+	    String reversed=new StringBuilder(input).reverse().toString();
+	    if(input.equals(reversed)){
+	        System.out.println(input+"is a palindrome");
+	    }else{
+	        System.out.println(input+"is not a palindrome");
+	    }
+	   sc.close();
+	   
+	}
+}
